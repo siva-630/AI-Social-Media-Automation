a04c2465d16ed333715571b53bc1f8358dda0f71
siva-630 <siva630029@gmail.com>
2026-07-28 21:49:47 +0530

    feat: configure Vite with React compiler and Tailwind support and add Vercel deployment headers

--- CLIENT/vite.config.ts
+++ CLIENT/vite.config.ts
@@ -6,4 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+    server: {
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+            "Cross-Origin-Embedder-Policy": "unsafe-none",
+        },
+    },
 });
